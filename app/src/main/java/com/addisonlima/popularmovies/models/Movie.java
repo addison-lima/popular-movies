96cf180df271c48a7628eabf038969235aac7e70
Addison Lima <alima@Addisons-MacBook-Air.local>
2019-08-11 21:00:48 -0400

    remove unused methods

--- app/src/main/java/com/addisonlima/popularmovies/models/Movie.java
+++ app/src/main/java/com/addisonlima/popularmovies/models/Movie.java
@@ -50,48 +50,24 @@ public class Movie implements Parcelable {
         return mTitle;
     }
 
-    public void setTitle(String title) {
-        mTitle = title;
-    }
-
     public String getOriginalTitle() {
         return mOriginalTitle;
-    }
-
-    public void setOriginalTitle(String originalTitle) {
-        mOriginalTitle = originalTitle;
     }
 
     public String getPosterPath() {
         return "http://image.tmdb.org/t/p/w500" + mPosterPath;
     }
 
-    public void setPosterPath(String posterPath) {
-        mPosterPath = posterPath;
-    }
-
     public String getOverview() {
         return mOverview;
-    }
-
-    public void setOverview(String overview) {
-        mOverview = overview;
     }
 
     public String getVoteAverage() {
         return mVoteAverage;
     }
 
-    public void setVoteAverage(String voteAverage) {
-        mVoteAverage = voteAverage;
-    }
-
     public String getReleaseDate() {
         return mReleaseDate;
-    }
-
-    public void setReleaseDate(String releaseDate) {
-        mReleaseDate = releaseDate;
     }
 
     @Override
